f3bc2a6c16b42cd261a497ca06996c11dd7af591
Marti <martina.klammer94@gmail.com>
2026-02-11 12:39:29 +0100

    Created CSS file and refresh button

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+<<<<<<< HEAD
 function addToDo() {
   toDoList.innerHTML +=
     '<li><input type="checkbox">' + toDoField.value + "</li>";
   toDoField.value = "";
 }
+=======
+function addToDo() {
+  toDoList.innerHTML +=
+    '<li><input type="checkbox">' + toDoField.value + "</li>";
+  toDoField.value = "";
+}
+
+function refresh() {
+  toDoList.innerHTML = "";
+}
+>>>>>>> 6cac914 (Created CSS file and refresh button)
